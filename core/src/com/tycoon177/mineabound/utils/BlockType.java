c75fff9f218589e0acb3493e5b106e72d2ec1a30
Ben McHone <benjamin.mchone@gmail.com>
2015-02-26 09:35:53 -0600

    changed to a better dirt texture

--- core/src/com/tycoon177/mineabound/utils/BlockType.java
+++ core/src/com/tycoon177/mineabound/utils/BlockType.java
@@ -3,7 +3,7 @@ package com.tycoon177.mineabound.utils;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public enum BlockType {
-	AIR(0, ""), DIRT(1, "dirt");
+	AIR(0, ""), DIRT(1, "grassCenter");
 
 	private TextureRegion sprite;
 	private int id;
